1c4e1c6d3d1a70f0c60d928750c767f4db85ee96
Thomas Petit-Jean <thomas.petitjean@aphp.fr>
2025-04-14 14:24:46 +0200

    update run.py

--- run.py
+++ run.py
@@ -8,18 +8,23 @@ tracks, df = clean(points)
 
 paths = []
 for trackID, d in df.groupby("trackID"):
-    path = d[["dbLat", "dbLon"]].to_dict(orient="list")
-    path = list(zip(path["dbLon"], path["dbLat"]))
+    track = tracks[tracks.trackID == trackID].squeeze()
+    values = d[["dbLat", "dbLon", "dist_total", "nSpeed", "duration_total"]].to_dict(orient="list")
+    path = list(zip(values["dbLon"], values["dbLat"]))
     timestamps = list(d.nTime - d.nTime.min())
     start = int(d.nTime.min())
-    vendor = 0
 
     paths.append(
         dict(
-            vendor=vendor,
             path=path,
             timestamps=timestamps,
+            speeds=values["nSpeed"],
+            distances=values["dist_total"],
             start=start,
+            duration_minutes=values["duration_total"],
+            trackID=trackID,
+            max_speed=float(track.max_speed),
+            formatted_start_date=track.formatted_start_date,
         )
     )
 
